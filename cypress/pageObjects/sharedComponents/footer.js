f52a50b16ea36669ea1c4a0b2c11e8464d982b77
Sam Greig <samgreig@AML0280.local>
2021-05-28 10:08:03 +0100

    feat: added tests for footer links

--- cypress/pageObjects/sharedComponents/footer.js
+++ cypress/pageObjects/sharedComponents/footer.js
@@ -4,11 +4,29 @@ class FooterObjects {
     }
 
     footerMenu() {
-        cy.get('.mtfh-footer__menu')
+        return cy.get('.mtfh-footer__menu')
+    }
+
+    reportAnIssueLink() {
+        return cy.contains('Report an issue')
+    }
+
+    makeSuggestionLink() {
+        return cy.contains('Make a suggestion')
     }
 
     footerIsDisplayed() {
         this.footerMain().should('be.visible')
+    }
+
+    footerLinksAreDisplayed() {
+        this.reportAnIssueLink().should('be.visible')
+        this.makeSuggestionLink().should('be.visible')
+    }
+
+    footerLinksAreCorrect() {
+        this.reportAnIssueLink().should('have.attr', 'href').and('equal', 'https://forms.gle/5kUGcRYFwwaZWrGs8')
+        this.makeSuggestionLink().should('have.attr', 'href').and('equal', 'https://forms.gle/yM7zCKYZcuVzSXkC6')
     }
 }
 export default FooterObjects
