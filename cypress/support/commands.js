d35c1d9b651ecbb731a09ae49c77f1b84607be85
Sam Greig <samgreig@AML0280.local>
2021-06-18 13:17:39 +0100

    fix: should fix the issue logging in on the pipeline run

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,11 @@
 import '@testing-library/cypress/add-commands';
 import 'cypress-axe'
 import 'cypress-audit/commands'
-const gssoTestKey = require('../../environment-config').gssoTestKey
+
+const environmentConfig = require('../../environment-config')
 
 Cypress.Commands.add('login', () => {
+    const gssoTestKey = environmentConfig.gssoTestKey
     cy.getCookies().should('be.empty')
     cy.setCookie('hackneyToken', gssoTestKey)
     cy.getCookie('hackneyToken').should('have.property', 'value', gssoTestKey)
