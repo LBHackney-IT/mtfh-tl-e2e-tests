e690e1dae5a1eff0607639bf1bd55cd4bc2ca537
Sam Greig <samgreig@AML0280.local>
2021-06-28 15:01:24 +0100

    fix: hopefully this is the correct config change

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,18 +1,11 @@
-import '@testing-library/cypress/add-commands';
 import 'cypress-axe'
 import 'cypress-audit/commands'
 
 const environmentConfig = require('../../environment-config')
-
 
 Cypress.Commands.add('login', () => {
     const gssoTestKey = environmentConfig.gssoTestKey
     cy.getCookies().should('be.empty')
     cy.setCookie('hackneyToken', gssoTestKey)
     cy.getCookie('hackneyToken').should('have.property', 'value', gssoTestKey)
-})
-
-Cypress.Commands.add('logout', () => {
-    cy.clearCookies()
-    cy.getCookies().should('be.empty')
-})
+}
