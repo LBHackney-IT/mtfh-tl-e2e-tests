11cf01c1f72c282b1a92b7a47c601bf31ce7cb69
Sam Greig <samgreig@AML0280.local>
2021-07-15 16:26:57 +0100

    fix: added correct urls for edit person ansd contact details

--- cypress/pageObjects/editPersonPage.js
+++ cypress/pageObjects/editPersonPage.js
@@ -2,7 +2,7 @@ const envConfig = require('../../environment-config')
 
 class EditPersonPageObjects {
     visit(record) {
-        cy.visit(`${envConfig.baseUrl}/${envConfig.personUrl}/${record}/add`)
+        cy.visit(`${envConfig.baseUrl}/${envConfig.personUrl}/${record}/edit`)
         cy.injectAxe()
     }
 }
