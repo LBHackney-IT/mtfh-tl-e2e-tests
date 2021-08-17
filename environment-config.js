97b754dcc81cff5d5d3bd53b362dd7c86369ff15
Sam Greig <samgreig@AML0280.local>
2021-08-17 16:35:33 +0100

    fix: changed the way the access token is passed into the runner

--- environment-config.js
+++ environment-config.js
@@ -7,23 +7,24 @@ let personCommentsUrl = "comment/person"
 let tenureUrl = "tenure"
 let rootComponentPort = "9000"
 let property = "property"
-let gssoTestKey = Cypress.env('E2E_ACCESS_TOKEN_LOCAL')
+// let gssoTestKey = Cypress.env('E2E_ACCESS_TOKEN_LOCAL')
+let gssoTestKey = Cypress.env('E2E_ACCESS_TOKEN')
 
 let baseUrl = `${rootUrl}:${rootComponentPort}`
 
 if (environment === 'development') {
     baseUrl = "https://manage-my-home-development.hackney.gov.uk"
-    gssoTestKey = Cypress.env('E2E_ACCESS_TOKEN_DEV')
+    // gssoTestKey = Cypress.env('E2E_ACCESS_TOKEN_DEV')
 }
 
 if (environment === 'staging') {
     baseUrl = "https://manage-my-home-staging.hackney.gov.uk"
-    gssoTestKey = Cypress.env('E2E_ACCESS_TOKEN_STAGING')
+    // gssoTestKey = Cypress.env('E2E_ACCESS_TOKEN_STAGING')
 }
 
 if (environment === 'production') {
     baseUrl = "https://manage-my-home.hackney.gov.uk"
-    gssoTestKey = Cypress.env('E2E_ACCESS_TOKEN_PROD')
+    // gssoTestKey = Cypress.env('E2E_ACCESS_TOKEN_PROD')
 }
 
 module.exports = {
