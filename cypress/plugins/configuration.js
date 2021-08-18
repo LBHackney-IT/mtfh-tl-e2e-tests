18e13df3f193e7b9a0c282a136d84ae1378a0444
Sam Greig <samgreig@AML0280.local>
2021-08-18 10:15:36 +0100

    fix: ecorrected env var usage

--- cypress/plugins/configuration.js
+++ cypress/plugins/configuration.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
-let url = "";
 
 const getConfiguration = async (env) => {
   let token = env.E2E_ACCESS_TOKEN_LOCAL;
-  url = `https://a9nuohv61k.execute-api.eu-west-2.amazonaws.com/development/api/v1/configuration?types=MMH`;
+  // url = `https://a9nuohv61k.execute-api.eu-west-2.amazonaws.com/development/api/v1/configuration?types=MMH`;
+  const url = env.FEATURE_TOGGLE_ENDPOINT;
 
   if (env.ENVIRONMENT === "development") {
     token = env.E2E_ACCESS_TOKEN_DEV;
@@ -11,12 +11,12 @@ const getConfiguration = async (env) => {
 
   if (env.ENVIRONMENT === "staging") {
     token = env.E2E_ACCESS_TOKEN_STAGING;
-    url = `https://4arxz3ei3a.execute-api.eu-west-2.amazonaws.com/staging/api/v1/configuration?types=MMH`;
+    // url = `https://4arxz3ei3a.execute-api.eu-west-2.amazonaws.com/staging/api/v1/configuration?types=MMH`;
   }
 
   if (env.ENVIRONMENT === "production") {
     token = env.E2E_ACCESS_TOKEN_PROD;
-    url = `https://a9nuohv61k.execute-api.eu-west-2.amazonaws.com/development/api/v1/configuration?types=MMH`;
+    // url = `https://a9nuohv61k.execute-api.eu-west-2.amazonaws.com/development/api/v1/configuration?types=MMH`;
   }
 
   const response = await axios.get(encodeURI(url), {
