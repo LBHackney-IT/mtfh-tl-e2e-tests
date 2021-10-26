075ce5d4411979ce73bf477b09714f001d4ad6e7
Sam Greig <samgreig@AML0280.local>
2021-10-26 12:30:16 +0100

    chore: cleaned up unused comment

--- api/contact-details.js
+++ api/contact-details.js
@@ -11,9 +11,6 @@ const deleteContactDetails = async (contactDetailsId, targetId) => {
     return response
 }
 
-
-// https://gos4l9my1a.execute-api.eu-west-2.amazonaws.com/development/api/v1/contactDetails?id=a4e8d069-fe75-4fcc-b694-e8b26e6156bb&targetId=279bf08c-0c9e-4d81-e24a-8930e8b37a68
-
 module.exports = {
     getContactDetails,
     deleteContactDetails
