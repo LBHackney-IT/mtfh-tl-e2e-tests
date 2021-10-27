d1f25a33d0ec395a12a27fd4c6f75dd625056ac9
YukiK <yuki.kitaoka@amido.com>
2021-10-27 11:20:12 +0100

    Updated model to include household members

--- api/models/requests/addTenureModel.js
+++ api/models/requests/addTenureModel.js
@@ -3,6 +3,16 @@ const createTenureModel = {
         "code": "FRS",
         "description": "Freehold (Serv)"
     },
+    "householdMembers": [
+        {
+        "id": "5270ebf3-7ec5-4825-8273-6843ad7781db",
+        "type": "person",
+        "fullName": "Dolphin Rio",
+        "isResponsible": true,
+        "dateOfBirth": "1962-05-10",
+        "personTenureType": "Tenant"
+        }
+    ],
     "tenuredAsset": {
         "id": "667c6809-d27b-40c6-a263-48670e253b2f",
         "type": "Dwelling",
