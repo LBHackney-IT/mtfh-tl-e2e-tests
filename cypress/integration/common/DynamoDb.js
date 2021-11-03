27a7fe834cf6e881bb370edce33d789f66501d97
YukiK <yuki.kitaoka@amido.com>
2021-11-03 14:09:06 +0000

    Updated delete method

--- cypress/integration/common/DynamoDb.js
+++ cypress/integration/common/DynamoDb.js
@@ -7,34 +7,40 @@ import {
 } from "cypress-cucumber-preprocessor/steps";
 
 
+
 Given("I can access to DynamoDB in AWS", async () => {
   cy.log("Connectingi to DynamoDB....")
   var AWS = require("aws-sdk");
   AWS.config.update({
     region: "us-west-2",
-    endpoint: "http://localhost:8000"
+    endpoint: "http://localhost:8000",
+    accessKeyId: "localKey",
+    secretAccessKey: "localSecret"
   });
   var tableName = "TenureInformation"
   var docClient = new AWS.DynamoDB.DocumentClient();
+  var id = "60a6c8fc-3267-4c99-ae71-a159f6f87b28"
+  deleteOrder(id)
+
+  function deleteOrder(id) {
+    return docClient
+     .delete({
+      TableName: "TenureInformation",
+      Key: {
+       id: id
+      }
+     })
+     .promise()
+     .then(result => {
+      console.log("tenure is deleted!", result);
+      return result;
+     })
+     .catch(deleteError => {
+      console.log(`tenure is not deleted :(`, deleteError);
+      throw deleteError;
+     });
+   }
+
 });
 
 
-
-// function deleteOrder(orderId) {
-//   return docClient
-//    .delete({
-//     TableName: "pizza-orders",
-//     Key: {
-//      orderId: orderId
-//     }
-//    })
-//    .promise()
-//    .then(result => {
-//     console.log("Order is deleted!", result);
-//     return result;
-//    })
-//    .catch(deleteError => {
-//     console.log(`Oops, order is not deleted :(`, deleteError);
-//     throw deleteError;
-//    });
-//  }
