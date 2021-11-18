a33ccc8f39bb09385cec33bf534647a1395dc82d
Sam Greig <samgreig@AML0280.local>
2021-11-18 11:17:36 +0000

    feat: added initial update function to set tenure to null within an asset record

--- cypress/integration/common/DynamoDb.js
+++ cypress/integration/common/DynamoDb.js
@@ -3,15 +3,16 @@ const accessKeyId = Cypress.env('AWS_ACCESS_KEY_ID')
 const secretAccessKey= Cypress.env('AWS_SECRET_ACCESS_KEY')
 const AWS = require("aws-sdk");
 
-const deleteRecordFromDynamoDB = async (tableName, id)=>{
-    var docClient = new AWS.DynamoDB.DocumentClient();
-  
-    AWS.config.update({
-      region: 'eu-west-2',
-      accessKeyId: accessKeyId,
-      secretAccessKey: secretAccessKey
-    });
+var docClient = new AWS.DynamoDB.DocumentClient();
 
+AWS.config.update({
+  region: 'eu-west-2',
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey
+});
+
+const deleteRecordFromDynamoDB = async (tableName, id) => {
+  
   deleteRecord(id)
 
   function deleteRecord(id) {
@@ -34,6 +35,33 @@ const deleteRecordFromDynamoDB = async (tableName, id)=>{
   }
 }
 
+const conditionallyRemoveAttachedTenuresFromDynamoDB = async (tableName, id) => {
+
+  updateRecord(id)
+
+  function updateRecord(id) {
+    return docClient
+    .update({
+      TableName: tableName,
+      Key: {
+        id: id
+      },
+      UpdateExpression: "SET tenure = Null",
+      ConditionExpression: "tenure = Null"
+    })
+    .promise()
+    .then(result => {
+      console.log("The tenure(s) have been removed: ", result);
+      return result;
+    })
+    .catch(deleteError => {
+      console.log("The tenure(s) have not been removed: ", deleteError);
+      throw deleteError;
+    })
+  }
+}
+
 module.exports = {
+  conditionallyRemoveAttachedTenuresFromDynamoDB,
   deleteRecordFromDynamoDB
 }
