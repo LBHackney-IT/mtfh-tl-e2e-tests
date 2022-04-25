6ca70411f4906945c09a73ebfa70f7032242f360
Ray Mitchell <ray@Rays-Air-2.broadband>
2022-04-25 13:31:02 +0100

    cypress issue

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,29 +1,24 @@
-import "@testing-library/cypress/add-commands";
-import "cypress-axe";
-import "cypress-audit/commands";
-import "cypress-localstorage-commands";
+import '@testing-library/cypress/add-commands';
+import 'cypress-axe'
+import 'cypress-audit/commands'
+import "cypress-localstorage-commands"
 
-const environmentConfig = require("../../environment-config");
+const environmentConfig = require('../../environment-config')
 
-Cypress.Commands.add("login", () => {
-  const gssoTestKey = environmentConfig.gssoTestKey;
-  cy.log(gssoTestKey);
-  cy.getCookies().should("be.empty");
-  cy.setCookie("hackneyToken", gssoTestKey);
-  cy.getCookie("hackneyToken").should("have.property", "value", gssoTestKey);
-  cy.log(Cypress.config("featureToggles"));
+Cypress.Commands.add('login', () => {
+  const gssoTestKey = environmentConfig.gssoTestKey
+  cy.log(gssoTestKey)
+  cy.getCookies().should('be.empty')
+  cy.setCookie('hackneyToken', gssoTestKey)
+  cy.getCookie('hackneyToken').should('have.property', 'value', gssoTestKey)
+  cy.log(Cypress.config("featureToggles"))
   window.localStorage.setItem(
-    "features",
-    JSON.stringify(Cypress.config("featureToggles"))
+      "features",
+      JSON.stringify(Cypress.config("featureToggles"))
   );
-});
+})
 
-Cypress.on("uncaught:exception", (err, runnable) => {
-  console.log(err);
-  return false;
-});
-
-Cypress.Commands.add("logout", () => {
-  cy.clearCookies();
-  cy.getCookies().should("be.empty");
+Cypress.Commands.add('logout', () => {
+  cy.clearCookies()
+  cy.getCookies().should('be.empty')
 });
