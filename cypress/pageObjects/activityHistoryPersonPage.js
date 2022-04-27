01860cfe1bda36bcd98b0dad9af7aab120950dcb
Ray Mitchell <ray@Rays-Air-2.broadband>
2022-04-27 15:49:42 +0100

    cypress-activityhistoryperson

--- cypress/pageObjects/activityHistoryPersonPage.js
+++ cypress/pageObjects/activityHistoryPersonPage.js
@@ -7,7 +7,7 @@ class ActivityHistoryPageObjects {
     }
 
     activityTable() {
-        return cy.get('[data-testid="person-activities"]').should('be.visible');
+        return cy.get('[data-testid="person-activities"]')
     }
 
     activityTableRow() {
