f72556f3ecf3ab825ce13618f612fcb536ccf9a1
Ray Mitchell <ray@Rays-Air-2.broadband>
2022-04-27 17:11:23 +0100

    cypress-activityhistoryperson

--- cypress/integration/activityHistoryPerson/activityHistoryPerson.js
+++ cypress/integration/activityHistoryPerson/activityHistoryPerson.js
@@ -9,8 +9,7 @@ Given('I go to the activity history for {string}', (person) => {
 })
 
 Then('the activity history is displayed', () => {
-    cy.wait(10000);
-    activityHistory.activityTable().should('be.visible')
+    activityHistory.activityTable().should('exist')
 })
 
 Then('I click close activity history', () => {
