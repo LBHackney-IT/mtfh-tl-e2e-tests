16214fff0e8178e5b6c9d880a4542809132962cd
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-08-04 11:15:39 +0100

    another fix to dynamodb client

--- cypress/integration/common/DynamoDb.js
+++ cypress/integration/common/DynamoDb.js
@@ -4,25 +4,24 @@ const deleteRecordFromDynamoDB = async ({tableName, key}) => {
   const accessKeyId = Cypress.env('AWS_ACCESS_KEY_ID')
   const secretAccessKey = Cypress.env('AWS_SECRET_ACCESS_KEY')
 
-  const docClient = new DynamoDB.DocumentClient({
+  AWS.config.update({
     region: 'eu-west-2',
     accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKey
-  })
+    secretAccessKey: secretAccessKey,
+  });
+  
+  const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-2'});
 
   deleteRecord(tableName, key)
 
   async function deleteRecord(tableName, key) {
     try {
-        const result_1 = await docClient
-            .delete({
-                    TableName: tableName,
-                    Key: key,
-                    })
-            .promise()
+        const result = await docClient.delete({ TableName: tableName, Key: key })
+                                        .promise()
 
-        console.log(`A record has been deleted from DynamoDb table ${tableName}: `, result_1,)
-        return result_1
+        console.log(`A record has been deleted from DynamoDb table ${tableName}: `, result)
+        return result;
+
     } catch (deleteError) {
         console.log('A record is not deleted: ', deleteError)
         throw deleteError
