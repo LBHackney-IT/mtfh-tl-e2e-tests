f8272e2075ea30c5dcfd04c079460a031c1a51bd
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-08-05 13:29:44 +0100

    Update dynamodb.js

--- cypress/integration/common/DynamoDb.js
+++ cypress/integration/common/DynamoDb.js
@@ -1,25 +1,32 @@
-import AWS from "aws-sdk";
+import AWS from 'aws-sdk';
 
-export const deleteRecordFromDynamoDB = async ({tableName, key}) => {
-  
-  AWS.config.getCredentials(function(err) {
-    if (err) console.log(err.stack); // credentials not loaded
-  })
+const deleteRecordFromDynamoDB = async ({tableName, key}) => {
+  const accessKeyId = Cypress.env('AWS_ACCESS_KEY_ID')
+  const secretAccessKey = Cypress.env('AWS_SECRET_ACCESS_KEY')
+  const sessionToken = Cypress.env('AWS_SESSION_TOKEN')
+
+  AWS.config.update({
+    region: 'eu-west-2',
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
+    sessionToken: sessionToken
+  });
   
   const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-2'});
-  
-  try {
-      const result = await docClient.delete({ TableName: tableName, Key: key })
-                                    .promise();
-      console.log(`A record has been deleted from DynamoDb table ${tableName}: `, result)
-      return result;
 
-  } catch (deleteError) {
-      console.log('A record is not deleted: ', deleteError)
-      throw deleteError
-  }
+    try {
+        const result = await docClient.delete({ TableName: tableName, Key: key })
+                                      .promise();
+
+        console.log(`A record has been deleted from DynamoDb table ${tableName}: `, result)
+        return result;
+
+    } catch (deleteError) {
+        console.log('A record is not deleted: ', deleteError)
+        throw deleteError
+    }
 }
 
-export default { 
+export default {
   deleteRecordFromDynamoDB
-};
+}
