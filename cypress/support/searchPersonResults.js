863173efde4cce2c6792895b89a207949ecdc858
karunak21 <karuna.kanumuri@foundry4.com>
2022-08-22 12:56:27 +0100

    change of name feature

--- cypress/support/searchPersonResults.js
+++ cypress/support/searchPersonResults.js
@@ -1,13 +1,17 @@
 
+const personSearchEndpointStaging = Cypress.env('HOUSE_SEARCH_ENDPOINT')
+
+
 export const searchPersonResults = (searchPerson) => {
     cy.request({
         method:"GET",
-        url:`https://y1e46yws9c.execute-api.eu-west-2.amazonaws.com/development/api/v1/search/persons?isDesc=true&page=1&pageSize=12&searchText=${searchPerson}`,
-       //staging next line
-        // url: `https://v4xprqejik.execute-api.eu-west-2.amazonaws.com/${Cypress.env('ENVIRONMENT')}/api/v1/search/persons?isDesc=true&page=1&pageSize=12&searchText=${searchPerson}`,
+       // url:`https://y1e46yws9c.execute-api.eu-west-2.amazonaws.com/development/api/v1/search/persons?isDesc=true&page=1&pageSize=12&searchText=${searchPerson}`,
+        url:`${personSearchEndpointStaging}/search/persons?isDesc=true&page=1&pageSize=12&searchText=${searchPerson}`,
+       // url: `https://v4xprqejik.execute-api.eu-west-2.amazonaws.com/staging/api/v1/search/persons?isDesc=true&page=1&pageSize=12&searchText=${searchPerson}`,
         failOnStatusCode: false,
         headers:{
-            Authorization: `Bearer ${Cypress.env('E2E_ACCESS_TOKEN_DEV')}`
+           // Authorization: `Bearer ${Cypress.env('E2E_ACCESS_TOKEN_DEV')}`
+            Authorization: `Bearer ${Cypress.env('E2E_ACCESS_TOKEN_STAGING')}`
         }
     }).then(result=> {
         expect(result.status).to.equal(200);
