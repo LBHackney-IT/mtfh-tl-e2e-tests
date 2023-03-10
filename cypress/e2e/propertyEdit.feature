d141c33eca5753450f3b3f9b7177cd662bd5b1d5
ange47rm <ange47rm@hotmail.com>
2023-03-10 15:37:51 +0000

    Added tests for Edit property address feature

---
+++ cypress/e2e/propertyEdit.feature
@@ -0,0 +1,60 @@
+@authentication
+@common
+@processes
+@property
+@root
+
+Feature: Property Edit
+    Edit property address
+
+    Background:
+        Given I am logged in
+
+    @SmokeTest
+    Scenario Outline: 'Edit address details button is disabled if the property has no valid UPRN'
+        Given I am on the MMH home page
+        When I enter "<propertyName>" as search criteria
+        And I select "<optionType>" and click on search button
+        Then I am on the Property search results page for "<propertyName>"
+        When I select a property
+        Then if the property has no valid UPRN, I can see a disabled button that says 'Cannot edit: UPRN missing'
+        Examples:
+            | propertyName | optionType |
+            | GGE          | property   |
+
+    @SmokeTest
+    Scenario Outline: 'Edit address details button is enabled if the property has a valid UPRN'
+        Given I am on the MMH home page
+        When I enter "<propertyName>" as search criteria
+        And I select "<optionType>" and click on search button
+        Then I am on the Property search results page for "<propertyName>"
+        When I select a property
+        Then if the property has a valid UPRN, I can see a button that says 'Edit address details'
+        Examples:
+            | propertyName      | optionType |
+            | 51 GREENWOOD ROAD | property   |
+
+    @SmokeTest
+    Scenario Outline: 'Edit property address page elements are present'
+        Given I am on the MMH 'Edit property address' page
+        Then I should see the heading 'Edit property address', and property details for the 'Suggestion from the Local Gazetteer' and the 'Current address'
+        And the 'Update to this address' and 'Cancel edit address' buttons are present, along with a 'Back to asset' link at the top
+        Examples:
+            | propertyName      | optionType |
+            | 51 GREENWOOD ROAD | property   |
+
+    @SmokeTest
+    Scenario Outline: 'Edit property address - patch address is successful'
+        Given I am on the MMH 'Edit property address' page
+        And I edit the address line 1 of the address
+        Then I click on 'Update to this address' button, and the PATCH request is successful
+        And I can see the address line 1 of the 'Current address' has changed successfully
+        And I can see a success message at the top of the screen
+        And the 'Update to this address' button should be disabled
+
+    @SmokeTest
+    Scenario Outline: 'Edit property address - patch address is not successful'
+        Given I am on the MMH 'Edit property address' page
+        And I edit the address line 1 of the address
+        Then I click on 'Update to this address' button, and the PATCH request fails
+        And I should see and error indicating that the request failed
---
+++ cypress/e2e/propertyEdit/propertyEdit.js
@@ -0,0 +1,97 @@
+import { When, Then, And, Given } from "@badeball/cypress-cucumber-preprocessor";
+import { baseUrl } from "../../../environment-config";
+
+import PropertyPageObjects from "../../pageObjects/propertyPage";
+
+const assetFixtureJson = require('../../fixtures/asset.json');
+const addressFixtureJson = require('../../fixtures/address.json');
+
+const propertyPage = new PropertyPageObjects();
+
+const propertyGuid = assetFixtureJson.id
+
+// Hardcoded URL pointing to a property with a valid UPRN for which editing is enabled
+const editPropertyAddressUrl = `${baseUrl}/property/edit/${propertyGuid}`
+
+const newAddressLine1Value = 'NEW ADDRESS LINE 1'
+
+When("I select a property", () => {
+    propertyPage.selectFirstRecord().click();
+})
+
+Given("I am on the MMH 'Edit property address' page", () => {
+    cy.intercept(
+        'GET', `*/api/v1/assets/${propertyGuid}`,
+        {
+            fixture: 'asset.json',
+        }
+    ).as('getAsset')
+
+    cy.intercept(
+        'GET', `*/api/v1/addresses?uprn=${assetFixtureJson.assetAddress.uprn}`,
+        {
+            fixture: 'address.json',
+        }
+    ).as('getAddress')
+
+    cy.visit(editPropertyAddressUrl)
+
+    cy.wait('@getAsset')
+    cy.wait('@getAddress')
+});
+
+Then("if the property has no valid UPRN, I can see a disabled button that says 'Cannot edit: UPRN missing'", () => {
+    cy.contains('Cannot edit: UPRN missing').should('be.visible')
+})
+
+Then("if the property has a valid UPRN, I can see a button that says 'Edit address details'", () => {
+    cy.contains('Edit address details').should('be.visible')
+})
+
+Then("I should see the heading 'Edit property address', and property details for the 'Suggestion from the Local Gazetteer' and the 'Current address'", () => {
+    cy.contains('Edit property address').should('be.visible')
+    cy.contains('Suggestion from the Local Gazetteer').should('be.visible')
+    cy.contains('Current address').should('be.visible')
+})
+
+And("the 'Update to this address' and 'Cancel edit address' buttons are present, along with a 'Back to asset' link at the top", () => {
+    cy.contains('Update to this address').should('be.visible')
+    cy.contains('Cancel edit address').should('be.visible')
+    cy.contains('Back to asset').should('be.visible')
+})
+
+And("I edit the address line 1 of the address", () => {
+    cy.get('[data-testid="addressLine1"]').clear().type(newAddressLine1Value)
+})
+
+Then("I click on 'Update to this address' button, and the PATCH request is successful", () => {
+    cy.intercept('PATCH', `*/api/v1/assets/${propertyGuid}/address`, { statusCode: 204 }).as('patchAddress')
+
+    cy.contains('Update to this address').click()
+
+    cy.wait('@patchAddress')
+})
+
+Then("I click on 'Update to this address' button, and the PATCH request fails", () => {
+    cy.intercept('PATCH', `*/api/v1/assets/${propertyGuid}/address`, { forceNetworkError: true }).as('patchAddress')
+
+    cy.contains('Update to this address').click()
+
+    cy.wait('@patchAddress')
+})
+
+And("I can see the address line 1 of the 'Current address' has changed successfully", () => {
+    cy.get('[data-testid="asset-address-line-one"]').should('have.value', newAddressLine1Value)
+})
+
+And("I can see a success message at the top of the screen", () => {
+    cy.contains('The asset address has been updated successfully.').should('be.visible')
+})
+
+And("the 'Update to this address' button should be disabled", () => {
+    cy.contains('Update to this address').should('be.disabled')
+})
+
+And("I should see and error indicating that the request failed", () => {
+    cy.contains('There was a problem amending this asset').should('be.visible')
+})
