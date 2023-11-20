50e23468d9ef2ee5e0fe8b4878d69e6803fb0ab1
Adam <adam.tracy20@gmail.com>
2023-11-20 11:40:42 +0000

    Remove timeout

--- cypress/pageObjects/workTrayPage.js
+++ cypress/pageObjects/workTrayPage.js
@@ -10,7 +10,7 @@ class WorkTrayPageObjects {
     };
 
     filterBy() {
-        return cy.contains('Filter by', { timeout: 10000 });
+        return cy.contains('Filter by');
     };
     filterShowDays() {
         return cy.get('[data-testid=mtfh-worktray-time-period]');
