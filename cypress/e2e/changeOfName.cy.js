f5ce0e17facd5eebb05c4c299f0a568496a56683
humulla <43747248+humulla@users.noreply.github.com>
2024-07-23 11:35:53 +0100

    fix tests

--- cypress/e2e/changeOfName.cy.js
+++ cypress/e2e/changeOfName.cy.js
@@ -53,7 +53,7 @@ describe("Change of Name Process", { tags: tags }, () => {
         page.checkBoxTenantInfo().click();
         page.buttonStartProcess().should('be.enabled');
         page.buttonStartProcess().click({ force: true });
-
+        cy.wait(1000)
 
         // Enter new name
         cy.contains("Enter tenant's new name").should('be.visible');
