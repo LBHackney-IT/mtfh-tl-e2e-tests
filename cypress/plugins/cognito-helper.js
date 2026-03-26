20fbacd433b3f300fcd381376fa4cddd9ff0ccc3
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2026-03-26 20:20:16 +0000

    Moaaaaaaaaar logging!

--- cypress/plugins/cognito-helper.js
+++ cypress/plugins/cognito-helper.js
@@ -6,11 +6,16 @@ async function fetchCognitoToken(env) {
     const username = env.E2E_USERNAME;
     const password = env.E2E_PASSWORD;
 
+    console.warn("Running 'fetchCognitoToken'");
+    console.log("Checking cognito env vars emptiness");
     if (!clientId || !username || !password || !region) {
         throw new Error("Missing Cognito login environment variables.");
     }
 
+    console.log("Env vars are not empty.");
+
     try {
+        console.log("Making a request for cognito token.");
         const response = await axios.post(`https://cognito-idp.${region}.amazonaws.com/`, {
             AuthFlow: 'USER_PASSWORD_AUTH',
             ClientId: clientId,
@@ -24,6 +29,13 @@ async function fetchCognitoToken(env) {
                 'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth',
             },
         });
+        console.log("Request done.");
+        console.log("Making a request for cognito token.");
+
+        console.log("Keys 'data':", Object.keys(response.data));
+        console.log("Keys 'auth result':", Object.keys(response.data.AuthenticationResult));
+        console.log("Some number of chars of token", response.data.AuthenticationResult.IdToken.slice(0,10));
+        console.log("End of 'fetchCognitoToken'.");
 
         return response.data.AuthenticationResult.IdToken;
     } catch (error) {
