d4642731e4c5b4fc890a4c936d5fbc731eeab57c
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2026-03-26 21:00:10 +0000

    feature toggles depend on having the right token

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -9,8 +9,8 @@ const { fetchFeatureToggleConfiguration } = require("./feature-toggle-config")
 const { setEnvironmentConfig } = require("./environment-config")
 
 module.exports = async (on, config) => {
-  config.featureToggles = (await fetchFeatureToggleConfiguration(config.env)) || {};
   config = await setEnvironmentConfig(on, config);
+  config.featureToggles = (await fetchFeatureToggleConfiguration(config.env)) || {};
   config = require('@cypress/grep/src/plugin')(config);
 
   on("before:browser:launch", (browser = {}, launchOptions) => {
