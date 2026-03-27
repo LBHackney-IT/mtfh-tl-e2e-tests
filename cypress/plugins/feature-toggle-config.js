61bde01ad2482ff1576775aa3a9168aca14abe7c
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2026-03-27 10:09:29 +0000

    catch block for almost no reason

--- cypress/plugins/feature-toggle-config.js
+++ cypress/plugins/feature-toggle-config.js
@@ -30,6 +30,17 @@ const fetchFeatureToggleConfiguration = async (config) => {
       Authorization: `Bearer ${token}`,
       "x-hackney-user": token
     },
+  }).catch(function (error) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
   });
 
   let featureToggleStore = {};
