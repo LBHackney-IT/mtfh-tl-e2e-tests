f40220c56b23c6cc27158d377d6c477c4f1e9529
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2026-05-06 11:39:18 +0100

    Remove duplicate `mainContent` edit person contact details page object method.

--- cypress/pageObjects/editPersonContactDetailsPage.js
+++ cypress/pageObjects/editPersonContactDetailsPage.js
@@ -1,20 +1,17 @@
 
 class EditPersonContactDetailsPageObjects {
     visit(record) {
-        cy.visit(`${Cypress.config("baseUrl")}/person/${record}`)
-        cy.injectAxe()
+        cy.visit(`${Cypress.config("baseUrl")}/person/${record}`);
+        cy.injectAxe();
     }
 
     editPersonContactDetails(record) {
-        cy.visit(`${Cypress.config("baseUrl")}/person/${record}/edit-contact-details`)
+        cy.visit(`${Cypress.config("baseUrl")}/person/${record}/edit-contact-details`);
     }
 
     mainContent() {
-        return cy.get('#main-content')
-    }
-
-    mainContent() {
-        return cy.get('#main-content')
+        return cy.get('#main-content');
     }
 }
-export default EditPersonContactDetailsPageObjects
+
+export default EditPersonContactDetailsPageObjects;
