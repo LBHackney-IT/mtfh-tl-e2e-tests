a96dabf2083f81453427d1ce2b3b41e6bb7fa33d
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2026-05-06 10:19:33 +0100

    Make feature toggles fetch run after the token is retrieved via cognito login.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -9,8 +9,8 @@ const { fetchFeatureToggleConfiguration } = require("./feature-toggle-config")
 const { setEnvironmentConfig } = require("./environment-config")
 
 module.exports = async (on, config) => {
-  config.featureToggles = (await fetchFeatureToggleConfiguration(config)) || {};
   config = await setEnvironmentConfig(on, config);
+  config.featureToggles = (await fetchFeatureToggleConfiguration(config)) || {};
   config = require('@cypress/grep/src/plugin')(config);
 
   on("before:browser:launch", (browser = {}, launchOptions) => {
