153fa0b8b34dd75832398234705b493d36f787fa
Marta Pederiva <86296201+martapederiva@users.noreply.github.com>
2026-07-09 16:38:16 +0100

    Deleting unneeded stuff

--- cypress/e2e/editPatches.cy.js
+++
@@ -1,6 +0,0 @@
-describe.skip("edit patches on property page", { tags: ["@property", "@authentication", "@common", "@root"] }, () => {
-    it("patch assignment editing was removed from the property view sidebar", () => {
-        // Patch edit UI (edit-assignment-button, patch-dropdown-options) is no longer
-        // shown on the property view. New asset patch assignment is covered separately.
-    });
-});
